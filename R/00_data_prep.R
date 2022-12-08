5f0f8df345a90c756b73f77004c864df5b970dec
clfo2014@gmail.com <clfo2014@gmail.com>
2022-12-07 20:12:01 -0500

    added country data to db

--- R/00_data_prep.R
+++ R/00_data_prep.R
@@ -42,14 +42,12 @@ retail_data_raw_tbl <- tbl(con, "retail_data_raw") %>%
 retail_data_clean_tbl <- retail_data_raw_tbl %>% 
     filter(!is.na(customer_id)) %>% 
     filter(quantity > 0) %>% 
+    mutate(sales = quantity * price) %>% 
     mutate(description = trimws(description, which = c("both"))) %>% 
     mutate(description = description %>% str_replace_all("[^[:alnum:]]", "")) %>% 
     filter(!country %in% c(
         "EIRE", "Channel Islands", "RSA", "West Indies"
-    )) %>% 
-    mutate(date = date(invoice_date)) %>% 
-    mutate(date = ymd(date)) %>% 
-    filter(date <= as.Date("2011-11-30"))
+    ))
 
 # dbWriteTable(con, "retail_data_clean_tbl", retail_data_clean_tbl, overwrite = TRUE)
 
@@ -59,7 +57,10 @@ retail_data_clean_tbl <- retail_data_raw_tbl %>%
 # - First Purchase Data
 # **********************************************************************************************
 
+# retail_data_clean_tbl <- tbl(con, "retail_data_clean_tbl")
+
 first_purchase_tbl <- retail_data_clean_tbl %>% 
+    mutate(date = lubridate::date(invoice_date)) %>% 
     select(date, customer_id) %>% 
     distinct() %>% 
     lazy_dt() %>% 
@@ -70,9 +71,18 @@ first_purchase_tbl <- retail_data_clean_tbl %>%
     mutate(flag = case_when(
         date < as.Date("2011-08-01") ~ "Old",
         TRUE                         ~ "New"
-    )) 
+    )) %>% 
+    select(-date)
 
 # dbWriteTable(con, "first_purchase_tbl", first_purchase_tbl, overwrite = TRUE)
 
+# **********************************************************************************************
+# DATA PREP 3 ----
+# - Country Data
+# **********************************************************************************************
 
+country_tbl <- retail_data_clean_tbl %>% 
+    select(customer_id, country) %>% 
+    distinct()
 
+# dbWriteTable(con, "country_tbl", country_tbl, overwrite = TRUE)
