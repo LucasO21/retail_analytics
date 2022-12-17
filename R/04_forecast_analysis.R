d1f3a70b5fa48e904df85f47b08ef10a3093a746
clfo2014@gmail.com <clfo2014@gmail.com>
2022-12-17 12:23:18 -0500

    added fitted models

---
+++ R/04_forecast_analysis.R
@@ -0,0 +1,219 @@
+# RETAIL ANALYTICS PROJECT ----
+# FORECAST SCRIPT ----
+# **** ----
+
+# ******************************************************************************
+# SETUP ----
+# ******************************************************************************
+
+# * Set Working Dir ----
+setwd(here::here("R"))
+
+# * Libraries ----
+library(tidyverse)
+library(janitor)
+library(timetk)
+library(lubridate)
+library(DBI)
+library(tidymodels)
+library(modeltime)
+library(rules)
+
+
+# ******************************************************************************
+# LOAD DATA ----
+# ******************************************************************************
+
+# * Setup DB Connection ----
+con <- dbConnect(RSQLite::SQLite(), dbname = "../data/database.db")
+dbListTables(con)
+
+# * First Purchase Data ----
+first_purchase_tbl <- tbl(con, "first_purchase_tbl") %>% collect() 
+
+# * Load Sales Data ----
+retail_data_clean_tbl <- tbl(con, "retail_data_clean_tbl") %>% 
+    collect() %>% 
+    mutate(invoice_date = lubridate::date(invoice_date)) %>% 
+    filter(invoice_date <= as.Date("2011-11-30"))
+
+
+# ******************************************************************************
+# DATA PREP ----
+# ******************************************************************************
+
+# * Sales by Country ----
+retail_data_clean_tbl %>% 
+    group_by(country) %>% 
+    summarise(
+        total_quantity = sum(quantity),
+        total_sales    = sum(sales)
+    ) %>% 
+    ungroup() %>% 
+    arrange(desc(total_sales)) %>% 
+    mutate(
+        pct_total_qty = total_quantity/sum(total_quantity),
+        pct_total_sales = total_sales/sum(total_sales)
+    )
+
+# * Summarize by Time ----
+daily_quantity_tbl <- retail_data_clean_tbl %>% 
+    mutate(country = ifelse(country == "United Kingdom", "United Kingdom", "All Others")) %>% 
+    group_by(country) %>% 
+    summarize_by_time(invoice_date, "day", total_quantity = sum(quantity)) %>% 
+    ungroup()
+
+# * Sales Trend ----
+daily_quantity_tbl %>% 
+    group_by(country) %>% 
+    plot_time_series(invoice_date, .value = total_quantity, .facet_ncol = 1)
+
+# * ACF & PACF
+daily_quantity_tbl %>% 
+    filter(country == "United Kingdom") %>% 
+    plot_acf_diagnostics(invoice_date, total_quantity)
+
+# * Params ----
+FORECAST_TIMEFRAME <- "day"
+FORECAST_HORIZON   <- 90
+
+# * Full Data ----
+full_data_tbl <- daily_quantity_tbl %>% 
+    mutate_if(is.character, as.factor) %>% 
+    
+    # Transformations
+    mutate(total_quantity = log1p(total_quantity)) %>% 
+    
+    # Apply Time Series Engineering
+    group_by(country) %>% 
+    pad_by_time(invoice_date, .by = FORECAST_TIMEFRAME, .pad_value = 0) %>% 
+    
+    # Extend Into Future
+    future_frame(invoice_date, .length_out = FORECAST_HORIZON, .bind_data = TRUE) %>% 
+    
+    # Add Time Series Features
+    tk_augment_fourier(invoice_date, .periods = c(7, 14, 21, 28)) %>% 
+    tk_augment_lags(total_quantity, .lags = FORECAST_HORIZON) %>% 
+    tk_augment_slidify(
+        .value   = total_quantity_lag90,
+        .f       = ~ mean(.x, na.rm = TRUE),
+        .period  = c(7, 14, 21, 28),
+        .partial = TRUE,
+        .align   = "center"
+    ) %>% 
+    ungroup() %>% 
+    rowid_to_column(var = "row_id")
+
+# * Data Prepared ----
+data_prepared_tbl <- full_data_tbl %>% 
+    filter(! is.na(total_quantity)) %>% 
+    drop_na()
+
+# * Future Data ----
+future_data_tbl <- full_data_tbl %>% 
+    filter(is.na(total_quantity))
+
+
+# ******************************************************************************
+# TIME SPLIT ----
+# ******************************************************************************
+splits <- data_prepared_tbl %>% 
+    time_series_split(invoice_date, assess = FORECAST_HORIZON, cumulative = TRUE)
+
+train_tbl <- training(splits)
+test_tbl  <- testing(splits)
+
+
+# ******************************************************************************
+# RECIPE ----
+# ******************************************************************************
+recipe_spec <- recipe(total_quantity ~., data = train_tbl) %>% 
+    step_timeseries_signature(invoice_date) %>% 
+    step_rm(matches("(.iso)|(.xts)|(hour)|(minute)(second)(am.pm)")) %>% 
+    step_dummy(all_nominal(), one_hot = TRUE) %>% 
+    step_normalize(date_index.num, date_year)
+
+recipe_spec %>% prep() %>% juice() %>% glimpse()
+
+# ******************************************************************************
+# MODELING ----
+# ******************************************************************************
+
+# * Prophet ----
+wflw_fit_prophet <- workflow() %>% 
+    add_model(
+        spec = prophet_reg() %>% set_engine("prophet")
+    ) %>% 
+    add_recipe(recipe_spec) %>% 
+    fit(train_tbl)
+
+# * Prophet Boost ----
+wflw_fit_phrophet_boost <- workflow() %>% 
+    add_model(
+        spec = prophet_boost(
+            seasonality_daily = F,
+            seasonality_weekly = F,
+            seasonality_yearly = F
+        ) %>% set_engine("prophet_xgboost")
+    ) %>% add_recipe(recipe_spec) %>% 
+    fit(train_tbl)
+
+# * Xgboost ----
+wflw_fit_xgboost <- workflow() %>% 
+    add_model(
+        spec = boost_tree() %>% set_mode("regression") %>% set_engine("xgboost")
+    ) %>% 
+    add_recipe(recipe_spec %>% step_rm(invoice_date)) %>% 
+    fit(train_tbl)
+
+# *  Random Forest ----
+wflw_fit_ranger <- workflow() %>% 
+    add_model(
+        spec = rand_forest(mode = "regression") %>% set_engine("ranger")
+    ) %>% 
+    add_recipe(recipe_spec %>% step_rm(invoice_date)) %>% 
+    fit(train_tbl)
+
+# Cubist ----
+wflw_fit_cubist <- workflow() %>% 
+    add_model(
+        spec = cubist_rules() %>% set_engine("Cubist")
+    ) %>% 
+    add_recipe(recipe_spec %>% step_rm(invoice_date)) %>% 
+    fit(train_tbl)
+
+# * Mars ----
+wflw_fit_mars <- workflow() %>% 
+    add_model(
+        spec = mars(mode = "regression") %>% set_engine("earth")
+    ) %>% 
+    add_recipe(recipe_spec %>% step_rm(invoice_date)) %>% 
+    fit(train_tbl)
+
+# * Accuracy Check (Fit Models) ----
+fit_models_tbl <- modeltime_table(
+    wflw_fit_prophet,
+    wflw_fit_xgboost,
+    wflw_fit_phrophet_boost,
+    wflw_fit_ranger,
+    wflw_fit_cubist,
+    wflw_fit_mars
+)
+
+fit_models_accuracy_tbl <- fit_models_tbl %>% 
+    modeltime_accuracy(test_tbl) %>% 
+    arrange(rmse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
